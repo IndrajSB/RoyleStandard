ba0827a46bf6b6e6f37d6126806973d89b8b6fbf
Indraj Bahra <indraj.bahra@googlemail.com>
2019-11-09 09:40:35 +0000

    updated webiste contents

--- contact.php
+++ contact.php
@@ -34,7 +34,7 @@
 
         } else {
 
-            $emailTo = "info@bullyfirmuk.co.uk";
+            $emailTo = "info@royalestandard.com";
 
             $subject = $_POST['subject'];
 
@@ -55,7 +55,7 @@
 <html lang="en" dir="ltr">
 <head>
   <meta charset="utf-8">
-  <title>ROYLE STANDARD</title>
+  <title>ROYALE STANDARD</title>
   <link rel="stylesheet" href="styles.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -72,7 +72,7 @@
   <body>
     <section id="nav-bar">
       <nav class="navbar navbar-expand-sm navbar-custom">
-          <a class="navbar-brand" href="#"><img src="img/RoyleStandardLogo.png"></a>
+          <a class="navbar-brand" href="#"><img src="img/RoyaleStandardLogo.png"></a>
 
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCustom">
               <i class="fa fa-bars fa-lg py-1 text-white"></i>
@@ -81,7 +81,7 @@
           <div class="navbar-collapse collapse" id="navbarCustom">
               <ul class="navbar-nav ml-auto">
                   <li class="nav-item active">
-                      <a class="nav-link" href="index.html">HOME</a>
+                      <a class="nav-link" href="home.html">HOME</a>
                   </li>
                   <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -92,16 +92,14 @@
                       <a class="dropdown-item" href="mortgages.html">MORTGAGES</a>
                       <a class="dropdown-item" href="insurance.html">INSURANCE</a>
                       <a class="dropdown-item" href="loans.html">2ND CHANGE LOANS</a>
+                      <a class="dropdown-item" href="offshore.html">OFFSHORE</a>
                     </div>
                   </li>
                   <li class="nav-item active">
                       <a class="nav-link" href="associates.html">ASSOCIATES</a>
                   </li>
-                  <li class="nav-item active">
-                    <a class="nav-link" href="calculator.html">CALCULATOR</a>
-                  </li>
                   <li class="nav-item">
-                        <a class="nav-link" href="contact.html">CONTACT</a>
+                        <a class="nav-link" href="contact.php">CONTACT</a>
                   </li>
               </ul>
           </div>
@@ -109,15 +107,14 @@
     </section>
 
     <!-- Page Content -->
-    <div class="container">
+    <section id="content-contact">
       <div class="row">
         <div class="col-md-8 mb-5">
           <h2>Your Enquiry</h2>
           <hr class="line">
-          <div class="container">
             <div id="error"></div>
               <form method="post">
-                <label>*Your email address submitted will only be shared to Royle Standard*</label>
+                <label>*Your email address submitted will only be shared to Royale Standard*</label>
                 <fieldset class="form-group">
                   <label for="email">Email address</label>
                   <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
@@ -137,72 +134,33 @@
                 <br>
                 <button type="submit" id="submit" class="btn btn-dark">Submit</button>
               </form>
-            </div>
         </div>
         <div class="col-md-4 mb-5">
           <h2>Contact Us</h2>
           <hr>
           <address>
-            <strong>Trinity House</strong>
-            <br>22 Pennine Close
-            <br>Oadby
+            <strong>Royale Standard</strong>
+            <br>2A Francis Street
             <br>Leicester
-            <br>LE2 4TB
+            <br>LE2 2BD
           </address>
           <address>
             <abbr title="Phone">Phone:</abbr>
-            +44 116 3323 475
+            +44 116 431 5735
             <br>
             <abbr title="Phone">Mobile:</abbr>
             +44 7746 679 350
             <br>
             <abbr title="Email">Email:</abbr>
-            <a href="mailto:#">info@roylestandard.com </a>
+            <a href="mailto: info@royalestandard.com">info@royalestandard.com </a>
           </address>
         </div>
       </div>
-    </div>
-      <!-- /.row -->
-
-    <!--CONTACT US-->
-    <section id="enquiry">
-
-     </section>
-
-    <!--<section id="contact">
-      <div class="container">
-        <h1 class="contact">Get In Touch With Us</h1>
-        <div class="row">
-          <div class="contact-info">
-
-            <div class="follow">
-              <i class="fa fa-id-badge"></i> <a>Mr Nat Thakor</a>
-            </div>
-
-            <div class="follow">
-              <i class="fa fa-phone"></i><a href="tel: +44 116 3323 475">+44 116 3323 475</a>
-            </div>
-
-            <div class="follow">
-              <i class="fa fa-mobile"></i><a href="tel: +44 7746 679 350">+44 7746 679 350</a>
-            </div>
-
-            <div class="follow">
-              <i class="fa fa-envelope"></i><a href="mailto: info@roylestandard.com ">info@roylestandard.com </a>
-            </div>
-
-            <!--<div class="follow">
-              <i class="fa fa-instagram"></i>  <a href="https://www.instagram.com/bullyfirmuk/?hl=en">Social Media Link</a>
-            </div>
-
-        </div>
-      </div>
-    </section>-->
+    </section>
 
     <!-- jQuery first, then Bootstrap JS. -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
-
 
     <script type="text/javascript">
 
@@ -240,7 +198,6 @@
 
               }
           })
-
     </script>
 
     <!-- Footer -->
@@ -257,7 +214,7 @@
 
           <div class="col-md-6 mb-md-0 mb-3">
             <h6>Disclaimer</h6>
-            <p class="isclaimer">‘Your home may be repossessed if you do not keep up repayments on a mortgage or other loans secured on it. ’Royle Standard is Introducer Appointed Representative of Watermead Financial Ltd authorised and regulated by the Financial Conduct Authority. FCA Reg: 615369 SFA, School Fee Adviser is a trading style of Watermead Financial Ltd.
+            <p class="isclaimer">‘Your home may be repossessed if you do not keep up repayments on a mortgage or other loans secured on it. ’Royale Standard is Introducer Appointed Representative of Watermead Financial Ltd authorised and regulated by the Financial Conduct Authority. FCA Reg: 615369 SFA, School Fee Adviser is a trading style of Watermead Financial Ltd.
             <br>'Some of the services shown are not regulated by the FCA.'
             <br>Credit Consumer Licence No. 629680/1
             <br>Data Protection Act 1998 - Registration No. Z1804091</p>
@@ -270,9 +227,9 @@
     <section id="footer">
       <footer>
         <div class="socials">
-          <a class="socials" href="mailto: "><i class="fa fa-envelope fa-lg"></i></a>
-          <a href="tel: "><i class="fa fa-phone fa-lg"></i></a>
-          <a href=""><i class="fa fa-instagram fa-lg"></i></a>
+          <a class="socials" href="mailto: info@royalestandard.com"><i class="fa fa-envelope fa-lg"></i></a>
+          <a href="tel: +44 774 667 9350"><i class="fa fa-phone fa-lg"></i></a>
+          <a href=""><i class="fa fa-twitter fa-lg"></i></a>
           <a href=""><i class="fa fa-linkedin fa-lg"></i></a>
         </div>
         <p class="footer-msg">Developed by ISB Developers</p>
